25a620d2486b09c87296572976b1c1072b248ae0
市川恭佑 (ebi) <github@mail.ebi-yade.com>
2021-06-18 17:46:07 +0900

    Add a test for Unmarshal on zero values of []byte type

--- field_bytes/zero_test.go
+++ field_bytes/zero_test.go
@@ -33,19 +33,37 @@ func TestZeroMarshal(t *testing.T) {
 		target   Gettable
 		expected string
 	}{
+		// Context: of a naked field
 		{
-			name:     "a naked field",
+			name:     "nil of a naked field",
 			target:   Naked{},
 			expected: `{"Field":null}`,
 		},
 		{
-			name:     "a default JSON tag",
+			name:     "[] of a naked field",
+			target:   Naked{Field: []byte("")},
+			expected: `{"Field":""}`,
+		},
+		// Context: of a field JSON-tagged "field"
+		{
+			name:     `nil of a field JSON-tagged "field"`,
 			target:   DefaultTag{},
 			expected: `{"field":null}`,
 		},
 		{
-			name:     "a JSON tag with omitempty",
+			name:     `[] of a field JSON-tagged "field"`,
+			target:   Naked{Field: []byte("")},
+			expected: `{"Field":""}`,
+		},
+		// Context: of a field JSON-tagged "field,omitempty"
+		{
+			name:     `nil of a field JSON-tagged "field,omitempty"`,
 			target:   Omitempty{},
+			expected: `{}`,
+		},
+		{
+			name:     `[] of a field JSON-tagged "field,omitempty"`,
+			target:   Omitempty{Field: []byte("")},
 			expected: `{}`,
 		},
 	}
@@ -64,4 +82,96 @@ func TestZeroMarshal(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestZeroUnmarshal(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name     string
+		input    string
+		target   Gettable
+		expected []byte
+	}{
+		// Context: to a struct with the naked field
+		{
+			name:     `omitted field to a struct with the naked field`,
+			input:    `{}`,
+			target:   &Naked{},
+			expected: nil,
+		},
+		{
+			name:     `"" to a struct with the naked field`,
+			input:    `{"Field":""}`,
+			target:   &Naked{},
+			expected: []byte(""),
+		},
+		{
+			name:     `null to a struct with the naked field`,
+			input:    `{"Field":null}`,
+			target:   &Naked{},
+			expected: nil,
+		},
+		// Context: to a struct with the field JSON-tagged "field"
+		{
+			name:     `omitted field to a struct with the field JSON-tagged "field"`,
+			input:    `{}`,
+			target:   &DefaultTag{},
+			expected: nil,
+		},
+		{
+			name:     `"" to a struct with the field JSON-tagged "field"`,
+			input:    `{"field":""}`,
+			target:   &DefaultTag{},
+			expected: []byte(""),
+		},
+		{
+			name:     `null to a struct with the field JSON-tagged "field"`,
+			input:    `{"field":null}`,
+			target:   &DefaultTag{},
+			expected: nil,
+		},
+		// Context: to a struct with the field JSON-tagged "field,omitempty"
+		{
+			name:     `omitted field to a struct with the field JSON-tagged "field,omitempty"`,
+			input:    `{}`,
+			target:   &Omitempty{},
+			expected: nil,
+		},
+		{
+			name:     `"" to a struct with the field JSON-tagged "field,omitempty"`,
+			input:    `{"field":""}`,
+			target:   &Omitempty{},
+			expected: []byte(""),
+		},
+		{
+			name:     `null to a struct with the field JSON-tagged "field,omitempty"`,
+			input:    `{"field":null}`,
+			target:   &Omitempty{},
+			expected: nil,
+		},
+	}
+
+	for _, v := range cases {
+		t.Run(v.name, func(t *testing.T) {
+			if err := json.Unmarshal([]byte(v.input), v.target); err != nil {
+				t.Fatalf("failed to parse JSON: %s", err)
+			}
+
+			expected := strOrNil(v.expected)
+			result := strOrNil(v.target.GetField())
+
+			if expected != result {
+				t.Fatalf(`expected field value is "%s", but detected value is "%s"`, expected, result)
+			}
+		})
+	}
+}
+
+func strOrNil(b []byte) string {
+	if b == nil {
+		return "<nil>"
+	}
+
+	return string(b)
 }
